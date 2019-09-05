112a9b33abdfd225a534fcce7494be6f06e1857c
Deanne Alabastro <aladeanra@gmail.com>
2019-09-05 22:23:48 +1200

    Add firebase config

---
+++ src/services/firebase.js
@@ -0,0 +1,25 @@
+import * as firebase from 'firebase';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID
+} from 'react-native-dotenv'
+
+export const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
+};
+
+// Initialize Firebase
+const Firebase = firebase.initializeApp(firebaseConfig)
+
+export default Firebase
