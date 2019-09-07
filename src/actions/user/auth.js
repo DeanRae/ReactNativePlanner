5f60cb63c68c85bd1fd9e2a8b5e1cf367ffe7826
Deanne Alabastro <aladeanra@gmail.com>
2019-09-07 17:53:24 +1200

    Add credit to code source/tut and add loginUser function

--- src/actions/user/auth.js
+++ src/actions/user/auth.js
@@ -1,6 +1,18 @@
 import { auth, firestore, serverTimestamp } from "../../services/firebase";
 import * as types from './actionTypes';
+/**
+ * Code adapted from: 
+ * https://github.com/skantus/react-native-firebase-redux-authentication/blob/master/app/actions/session/actions.js
+ */
 
+
+/**
+ * Registers the user and if successful, adds user credentials to the firestore
+ * database and then dispatches signupSuccess. Else on error dispatches
+ * sessionError
+ * @param {*} data expected data received should include email, password, 
+ *                 and name (name may be an empty string)
+ */
 export const signupUser = (data) => (dispatch) => {
     const { email, password, name } = data;
     auth.createUserWithEmailAndPassword(email, password)
@@ -9,7 +21,7 @@ export const signupUser = (data) => (dispatch) => {
             firestore
                 .doc(`/userProfile/${newUserCredential.user.uid}`)
                 .set({ email, name });
-            
+
             dispatch(signupSuccess(newUserCredential));
         })
         .catch(error => {
@@ -18,4 +30,24 @@ export const signupUser = (data) => (dispatch) => {
         });
 
 }
+
+/**
+ * Logs in the user. If successful, dispatches session success else
+ * on failure, dispatches session error
+ * @param {*} email 
+ * @param {*} password 
+ */
+export const loginUser = (data) => dispatch => {
+    const {email, password} = data;
+    auth
+        .signInWithEmailAndPassword(email, password)
+        .then(user => {
+            dispatch(sessionSuccess(user));
+        })
+        .catch(error => {
+            dispatch(sessionError(error.message));
+        });
+};
+
+
 
