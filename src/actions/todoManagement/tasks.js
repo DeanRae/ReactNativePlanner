9a720f9ac25e04bc650b4ce38e65a0711a16ac6a
Deanne Alabastro <aladeanra@gmail.com>
2019-09-11 17:03:05 +1200

    add action to getAllTasks

--- src/actions/todoManagement/tasks.js
+++ src/actions/todoManagement/tasks.js
@@ -1,5 +1,54 @@
-import { firestore, serverTimestamp } from "../../services/firebase";
+import { firestore, serverTimestamp } from "../../services/firebase"; 
 import * as types from './actionTypes';
 
+export const getAllTasks = () => (dispatch, getState) => {
+    dispatch(loading);
 
+    const { user } = getState();
+    let tasks = {};
+
+    firestore
+        .collection(`/userProfile/${user.uid}/tasks`)
+        .get()
+        .then(allTasks => {
+            if (!allTasks.empty()) {
+                allTasks.forEach(task => {
+                    let taskData = task.data();
+                    tasks[taskData.id] = {
+                        id: taskData.id,
+                        title: taskData.title,
+                        description: taskData.description,
+                        location: taskData.location,
+                        listId: taskData.listId,
+                        startTime: taskData.startTime,
+                        endTime: taskData.endTime,
+                        allDay: taskData.allDay,
+                        completionRate: taskData.completionRate,
+                        isCompleted: taskData.isCompleted,
+                        createdTimestamp: taskData.createdTimestamp,
+                        updatedTimestamp: taskData.updatedTimestamp
+                    }
+                });
+            }
+
+            dispatch(taskOperationSuccess(tasks));
+
+        })
+        .catch( error => {
+            console.log("error ", error.message);
+            dispatch(taskOperationError(error.message));
+        });
+}
+
+
+
+const taskOperationSuccess = tasks => ({
+    type: types.TASK_OPERATION_SUCCESS,
+    tasks
+})
+
+const taskOperationError = error => ({
+    type: types.TASK_OPERATION_ERROR,
+    error
+});
 
