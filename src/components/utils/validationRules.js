1f7e8ca09bf426f12abdb5ad16071e818a42603a
Deanne Alabastro <aladeanra@gmail.com>
2019-09-14 23:56:53 +1200

    Change validation messages

--- src/components/utils/validationRules.js
+++ src/components/utils/validationRules.js
@@ -1,32 +1,28 @@
 const validationRules = {
+  email: {
+    presence: {
+      allowEmpty: false,
+      message: '^Please enter an email address. '
+    },
     email: {
-      presence: {
-        allowEmpty: false,
-        message: '^Please enter an email address'
-      },
-      email: {
-        message: '^Invalid email address'
-      }
-    },
-    
-    password: {
-      presence: {
-        allowEmpty: false,
-        message: '^Please enter a password'
-      },
-      length: {
-        minimum: 5,
-        message: '^Your password must be at least 5 characters'
-      }
-    },
-
-    confirmPassword: {
-        presence: {
-            allowEmpty: false,
-            message: '^Please enter your password again'
-        },
-        equality: "password"
+      message: '^Invalid email address '
     }
+  },
+
+  password: {
+    presence: {
+      allowEmpty: false,
+      message: '^Please enter a password. '
+    },
+    length: {
+      minimum: 6,
+      message: '^Your password must be at least 6 characters '
+    }
+  },
+
+  confirmPassword: {
+    equality: "password"
   }
-  
-  export default validationRules;
+}
+
+export default validationRules;
