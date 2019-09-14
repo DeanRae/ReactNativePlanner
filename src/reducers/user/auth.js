03e8a236a43631e533031a7370fa5c54e80cda4c
Deanne Alabastro <aladeanra@gmail.com>
2019-09-14 23:47:05 +1200

    implement the reducer for user auth/profile

---
+++ src/reducers/user/auth.js
@@ -0,0 +1,62 @@
+import * as types from '../../actions/user/actionTypes';
+
+const initialState = {
+  restoring: false,
+  sessionLoading: false,
+  user: {},
+  sessionError: null,
+  logged: null,
+  registered: null
+};
+
+const auth = (state = initialState, action) => {
+  switch (action.type) {
+    case types.SESSION_RESTORING:
+      return { ...state, restoring: true };
+    case types.SESSION_LOADING:
+      return { ...state, restoring: false, sessionLoading: true, sessionError: null };
+    case types.SESSION_SUCCESS:
+      return {
+        ...state,
+        restoring: false,
+        sessionLoading: false,
+        user: action.user,
+        sessionError: null,
+        logged: true,
+      };
+    case types.SIGNUP_SUCCESS:
+      return {
+        ...state,
+        restoring: false,
+        sessionLoading: false,
+        user: action.user,
+        sessionError: null,
+        logged: true,
+      };
+    case types.SESSION_ERROR:
+      return {
+        ...state,
+        restoring: false,
+        sessionLoading: false,
+        user: null,
+        sessionError: action.error,
+        logged: null,
+      };
+    case types.ERROR_ACKNOWLEDGED:
+      return {
+        ...state,
+        error: null
+      };
+    case types.SESSION_LOGOUT:
+      return initialState;
+    case types.USER_PROFILE_MODIFIED:
+      if (!action.data) {
+        return state;
+      }
+      return {...state, user:{...state.user,...action.data}};
+    default:
+      return state;
+  }
+};
+
+export default auth;
