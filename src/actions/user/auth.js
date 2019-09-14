1c7dd5802a9857a3ceff36a26f6c89eb2e13e6a8
Deanne Alabastro <aladeanra@gmail.com>
2019-09-14 23:46:46 +1200

    Add user profile actions and ammend some of the actionType names

--- src/actions/user/auth.js
+++ src/actions/user/auth.js
@@ -1,4 +1,4 @@
-import { auth, firestore, serverTimestamp } from "../../services/firebase";
+import { auth, firestore, serverTimestamp, credential, currentUser } from "../../services/firebase";
 import * as types from './actionTypes';
 /**
  * Code adapted from: 
@@ -13,19 +13,26 @@ import * as types from './actionTypes';
  * @param {*} data expected data received should include email, password, 
  *                 and name (name may be an empty string)
  */
-export const signupUser = (data) => (dispatch) => {
+export const signupUser = (data) => dispatch => {
+    dispatch(sessionLoading());
     const { email, password, name } = data;
-    auth.createUserWithEmailAndPassword(email, password)
+
+    auth().createUserWithEmailAndPassword(email, password)
         .then(newUserCredential => {
-            // add user info to firestore database.
-            firestore
+            // add user info to firestore() database.
+            firestore()
                 .doc(`/userProfile/${newUserCredential.user.uid}`)
                 .set({ email, name });
 
-            dispatch(signupSuccess(newUserCredential));
+            // do the same to actual firebase user object
+            newUserCredential.user.updateProfile({
+                displayName: name
+            });
+
+            dispatch(signupSuccess(newUserCredential.user));
         })
         .catch(error => {
-            console.log(error);
+            console.log(error.message);
             dispatch(sessionError(error.message));
         });
 
@@ -33,21 +40,158 @@ export const signupUser = (data) => (dispatch) => {
 
 /**
  * Logs in the user. If successful, dispatches session success else
- * on failure, dispatches session error
+ * on failure, dispatches session
  * @param {*} email 
  * @param {*} password 
  */
 export const loginUser = (data) => dispatch => {
-    const {email, password} = data;
-    auth
+    dispatch(sessionLoading());
+
+    const { email, password } = data;
+
+    auth()
         .signInWithEmailAndPassword(email, password)
         .then(user => {
-            dispatch(sessionSuccess(user));
+            dispatch(sessionSuccess(user.user));
         })
         .catch(error => {
             dispatch(sessionError(error.message));
         });
 };
+
+export const errorDisplayed = () => dispatch => {
+    dispatch(errorAcknowledged());
+}
+
+export const restoreSession = () => dispatch => {
+    dispatch(sessionLoading());
+    dispatch(sessionRestoring());
+
+    auth()
+        .onAuthStateChanged(user => {
+
+            if (user) {
+
+                dispatch(sessionSuccess(user));
+
+            } else {
+
+                dispatch(sessionLogout());
+            }
+        });
+};
+
+export const logoutUser = () => dispatch => {
+    dispatch(sessionLoading());
+
+    auth()
+        .signOut()
+        .then(() => {
+            dispatch(sessionLogout());
+        })
+        .catch(error => {
+            dispatch(sessionError(error.message));
+        });
+};
+
+export const updateUserName = (name) => (dispatch, getState) => {
+    const { auth } = getState();
+    const user = { ...auth.user };
+
+    user
+        .updateProfile({
+            displayName: name
+        }).
+        then(() => {
+            firestore()
+                .doc(`/userProfile/${user.uid}`)
+                .update({ name })
+                .then(() => {
+                    dispatch(userProfileModified({ name: name }));
+                });
+        })
+        .catch(error => {
+            dispatch(sessionError(error.message));
+        });
+};
+
+export const updateUserEmail = (newEmail, password) => (dispatch, getState) => {
+    const { auth } = getState();
+    const user = { ...auth.user };
+
+    const cred = credential(user, password);
+
+    user
+        .reauthenticateAndRetrieveDataWithCredential(cred)
+        .then(() => {
+            user.updateEmail(newEmail).then(() => {
+                firestore()
+                    .doc(`/userProfile/${user.uid}`)
+                    .update({ email: newEmail });
+            });
+            dispatch(userProfileModified({ email: newEmail }));
+        })
+        .catch(error => {
+            dispatch(sessionError(error.message));
+        });
+};
+
+
+export const updatePassword = (newPassword, oldPassword) => (dispatch, getState) => {
+    const { auth } = getState();
+    const user = { ...auth.user };
+
+    const cred = credential(user, password);
+
+    user
+        .reauthenticateAndRetrieveDataWithCredential(cred)
+        .then(() => {
+            this.currentUser.updatePassword(newPassword).then(() => {
+                dispatch(userProfileModified(null));
+            });
+        })
+        .catch(error => {
+            dispatch(sessionError(error.message));
+        });
+
+};
+
+const userProfileModified = data => ({
+    type: types.USER_PROFILE_MODIFIED,
+    data
+})
+
+const sessionRestoring = () => ({
+    type: types.SESSION_RESTORING
+});
+
+const sessionLoading = () => ({
+    type: types.SESSION_LOADING
+});
+
+const sessionSuccess = user => ({
+    type: types.SESSION_SUCCESS,
+    user
+});
+
+const signupSuccess = user => ({
+    type: types.SIGNUP_SUCCESS,
+    user
+});
+
+const sessionError = error => ({
+    type: types.SESSION_ERROR,
+    error
+});
+
+const errorAcknowledged = () => ({
+    type: types.ERROR_ACKNOWLEDGED
+});
+
+const sessionLogout = () => ({
+    type: types.SESSION_LOGOUT
+});
+
 
 
 
