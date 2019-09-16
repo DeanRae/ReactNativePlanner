18afbceca5deb9726378e25324a37120808dd3d9
Deanne Alabastro <aladeanra@gmail.com>
2019-09-16 18:42:20 +1200

    implement signup using authform

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,132 +1,74 @@
 import React, { Component } from 'react';
-import { StyleSheet, ScrollView, SafeAreaView } from 'react-native';
-import { TextInput, Button, HelperText } from 'react-native-paper';
+import { Alert } from 'react-native';
 import { connect } from 'react-redux';
-import { signupUser } from '../actions/user/auth';
-import { validate } from '../components/utils/inputValidation';
-
-const error = {
-    general: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
-}
-
+import LoadingIndicator from '../components/LoadingIndicator';
+import { signupUser, errorDisplayed } from '../actions/user/auth';
+import AuthForm from '../components/AuthForm';
 class SignupScreen extends Component {
     static navigationOptions = () => ({
         title: 'Sign Up'
     });
 
-    constructor() {
-        super();
-        this.state = {
-            email: '',
-            password: '',
-            error: error
-        };
+    componentDidUpdate = (prevProps) => {
+        if (this.props.logged && !this.props.error) {
+            this.props.navigation.navigate('App');
+        }
+
+        if (this.props.error) {
+            Alert.alert(
+                'Signup Error',
+                this.props.error,
+            );
+
+            this.props.errorDisplayed();
+        }
     }
 
     onSubmit = (data) => {
-        this.setState({ error: error }); //clear out error messages
-
-        this.props.signupUser(data)
-            .then((data) => {
-                console.log("user created");
-                console.log(data);
-            })
-            .catch(this.onError)
-    }
-
-    onError = (errorData) => {
-        let errObj = {...this.state.error};
-
-        console.log("error obj " ,errObj);
-
-        if (errorData.hasOwnProperty("message")) {
-            errObj['general'] = errorData.message;
-            console.log("shouldnt be here");
-        } else {
-            console.log("At error sorting");
-            let keys = Object.keys(errorData);
-            keys.map((key, index) => {
-                errObj[key] = errorData[key];
-            })
-            console.log(errObj);
-        }
-        this.setState({ error: errObj });
-    }
-
-    validateInput = (fields = { email: this.state.email, password: this.state.password }) => {
-        let result = validate(fields);
-
-        if (result) {
-            this.onError(result);
-        } else {
-            this.setState({ error: error}); // reset errors if no validation errors
-        }
+        this.props.signup(data);
     }
 
     render() {
+        const error = {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        };
+        
+        const inputs = {
+            email: '',
+            password: '',
+            confirmPassword: '',
+            name: '',
+        };
+
+        const buttonDetails = [{
+            buttonTitle: 'Signup',
+            hasSolidColor: true,
+            onPressFunc: this.onSubmit
+        }];
+
         return (
-            <ScrollView contentContainerStyle={styles.parentView} scrollEnabled={false}>
-                <SafeAreaView style={styles.centered}>
-                    <TextInput
-                        mode='outlined'
-                        label='Email'
-                        keyboardType='email-address'
-                        value={this.state.email}
-                        autoCompleteType='email'
-                        autoCapitalize='none'
-                        onChangeText={email => { this.setState({ email }); console.log(email); this.validateInput({ email: email }) }}
-                        style={styles.formComponent}
-                    />
-                    <HelperText
-                        type="error"
-                        visible={this.state.error.email}
-                    >
-                        {this.state.error.email}
-                    </HelperText>
-                    <TextInput
-                        mode='outlined'
-                        label='Password'
-                        secureTextEntry={true}
-                        value={this.state.password}
-                        autoCompleteType='password'
-                        autoCapitalize='none'
-                        onChangeText={password => { this.setState({ password }); this.validateInput({ password: password }) }}
-                        style={styles.formComponent}
-                    />
-                    <HelperText
-                        type="error"
-                        visible={this.state.error.password}
-                    >
-                        {this.state.error.password}
-                    </HelperText>
-                    <Button style={styles.formComponent} contentStyle={{ height: 56 }} mode="contained" onPress={() => console.log(error)}>
-                        Login
-              </Button>
-
-                </SafeAreaView>
-            </ScrollView>
-
+            this.props.loading ? 
+            <LoadingIndicator /> : 
+            <AuthForm fields = {inputs} initErrors = {error} buttonDetails ={ buttonDetails}/>
         );
     }
 
 }
 
-const styles = StyleSheet.create({
-    parentView: {
-        flexGrow: 1,
-        justifyContent: 'space-between'
-    },
-    centered: {
-        flex: 1,
-        justifyContent: 'center',
-        margin: 10
-    },
-    formComponent: {
-        margin: 10,
-    },
+const mapStateToProps = ({ auth: { sessionLoading, sessionError, logged } }) => ({
+    loading: sessionLoading,
+    error: sessionError,
+    logged: logged
 });
 
-export default connect(null, { signupUser })(SignupScreen);
+const mapDispatchToProps = {
+    signup: signupUser,
+    errorDisplayed
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(SignupScreen);
