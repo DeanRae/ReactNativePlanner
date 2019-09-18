90d46904d6994269b5f4896519aac82ab2ca958d
Deanne Alabastro <aladeanra@gmail.com>
2019-09-18 23:58:34 +1200

    create calendar screen

---
+++ src/screens/CalendarScreen.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {View} from 'react-native';
+import { connect } from 'react-redux';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Header } from 'react-native-elements';
+import LoadingIndicator from '../components/LoadingIndicator';
+import { getAllTasks, addTask, deleteTask, editTask, errorDisplayed } from '../actions/todoManagement/tasks';
+import styles from '../components/utils/globalStyles';
+
+class CalendarScreen extends Component {
+    static navigationOptions = () => ({
+        title: 'Calendar'
+    });
+
+    componentDidUpdate = (prevProps) => {
+
+        if (this.props.error) {
+            Alert.alert(
+                'User Profile Error',
+                this.props.error,
+            );
+
+            this.props.errorDisplayed();
+        }
+    }
+
+    render() {
+        return (
+            <View >
+                <Header
+                    centerComponent={{ text: 'Calendar', style: styles.header }}
+                    backgroundColor="white"
+                    containerStyle={styles.headerContainer}
+                    statusBarProps={{ barStyle: 'dark-content' }}
+                />
+                {this.props.loading ? (
+                    <LoadingIndicator />
+                ) : null
+                }
+            </View>
+        );}
+        }
+        
+const mapStateToProps = ({tasks: {taskOperationLoading, taskOperationError, tasks} }) => ({
+                    loading: taskOperationLoading,
+                error: taskOperationError,
+                tasks: tasks.byId,
+                taskIds: tasks.allIds
+            });
+            
+const mapDispatchToProps = {
+                    getAllTasks,
+                    addTask,
+                    deleteTask,
+                    editTask,
+                    errorDisplayed
+                };
+                
+                export default connect(
+                    mapStateToProps,
+                    mapDispatchToProps
+)(CalendarScreen);
