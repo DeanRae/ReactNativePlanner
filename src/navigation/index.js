ba0556c0f9da4762c86ad87ef64b96c2ca701470
Deanne Alabastro <aladeanra@gmail.com>
2019-09-18 15:57:59 +1200

    Add icons to tabnav

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,6 +1,8 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import React from 'react';
 
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
@@ -8,9 +10,58 @@ import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 
-const AppNav = createBottomTabNavigator({ Home: HomeScreen, Profile: ProfileScreen });
-const AuthNav = createStackNavigator({ Login: LoginScreen, Signup: SignupScreen });
+/**
+ * Create navigation that's only accessible once authenticated 
+ */
+const AppNav = createBottomTabNavigator(
+  {
+    Home: HomeScreen,
+    Profile: ProfileScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) =>
+        getTabBarIcon(navigation, focused, tintColor),
+    })
+  }
+);
 
+/**
+ * Create navigation that's only accessible when unauthenticated
+ */
+const AuthNav = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Signup: SignupScreen
+  }
+);
+
+/**
+ * Create's an icon used in the tab menu based on the route
+ * @param {*} navigation 
+ * @param {*} focused 
+ * @param {*} tintColor 
+ */
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let IconComponent = Ionicons;
+  let iconName;
+
+  switch (routeName) {
+    case 'Home':
+      iconName = 'ios-home';
+      break;
+    case 'Profile':
+      iconName = 'ios-person';
+      break;
+  }
+
+  return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
+
+/**
+ * Combines all of the navigation
+ */
 export default createAppContainer(
   createSwitchNavigator(
     {
