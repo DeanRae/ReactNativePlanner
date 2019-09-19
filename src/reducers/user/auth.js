4b86519839fd10039cd25ad4e0c444dbdcc23db1
Deanne Alabastro <aladeanra@gmail.com>
2019-09-19 15:09:58 +1200

    fix error that returns null user and turn off loading when user profile modified

--- src/reducers/user/auth.js
+++ src/reducers/user/auth.js
@@ -41,9 +41,7 @@ const auth = (state = initialState, action) => {
         ...state,
         restoring: false,
         sessionLoading: false,
-        user: null,
         sessionError: action.error,
-        logged: null,
         resetPassword: false
       };
     case types.ERROR_ACKNOWLEDGED:
@@ -54,7 +52,7 @@ const auth = (state = initialState, action) => {
     case types.SESSION_LOGOUT:
       return initialState;
     case types.USER_PROFILE_MODIFIED:
-      return {...state, ...action.user};
+      return {...state, ...action.user, sessionLoading: false};
     case types.EMAILED_PASSWORD_RESET:
       return {
         ...state,
