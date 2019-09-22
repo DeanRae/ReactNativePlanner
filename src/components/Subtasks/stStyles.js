6edd0bef9bef09c31de2fc92e46bb364428d12ec
Deanne Alabastro <aladeanra@gmail.com>
2019-09-22 15:56:00 +1200

    create separate stylesheet for subtasks

---
+++ src/components/Subtasks/stStyles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+    subtaskHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: 50,
+        justifyContent: 'space-between',
+        margin: 10,
+        borderBottomColor: '#86939e',
+        borderBottomWidth: 1,
+    },
+    label: {
+        fontSize: 20,
+        color: '#86939e',
+        fontWeight: 'bold',
+    }
+});
