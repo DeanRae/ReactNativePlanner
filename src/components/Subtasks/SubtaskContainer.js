e1597634bd8dab1be2d092e54ca641c190c14e45
Deanne Alabastro <aladeanra@gmail.com>
2019-09-22 15:55:14 +1200

    render container header

--- src/components/Subtasks/SubtaskContainer.js
+++ src/components/Subtasks/SubtaskContainer.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import { View, Text, } from 'react-native';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/Ionicons';
 import PropTypes from 'prop-types';
+import styles from './stStyles';
 
-export default class SubtaskContainer {
+export default class SubtaskContainer extends Component {
 
     render() {
         return (
-            
+            <View>
+                <View style={styles.subtaskHeader}>
+                    <Text style={styles.label}>Subtasks</Text>
+                    <Button
+                        title='Add New Subtask'
+                        type='clear'
+                        // icon={
+                        //     <Icon
+                        //         name="ios-add"
+                        //         size={20}
+                        //         color='#007aff'
+                        //     />
+                        // }
+                        // iconRight
+                    />
+                </View>
+            </View>
         );
     }
 
 }
 
-Picker.propTypes = {
+SubtaskContainer.propTypes = {
     subtasks: PropTypes.arrayOf(
         PropTypes.shape({
             isChecked: PropTypes.bool.isRequired,
