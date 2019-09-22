2102a77ffd37c34bd90eae063ce93406f57682d2
Deanne Alabastro <aladeanra@gmail.com>
2019-09-22 21:46:45 +1200

    move rendering to functions and implement edit, add, and delete of subtasks

--- src/components/Subtasks/SubtaskContainer.js
+++ src/components/Subtasks/SubtaskContainer.js
@@ -1,43 +1,164 @@
 import React, { Component } from 'react';
 import { View, Text, } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, CheckBox } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Ionicons';
 import PropTypes from 'prop-types';
 import styles from './stStyles';
+import InputDialog from '../InputDialog';
 
+const initState = {
+    isEditTaskDialogVisible: false,
+    editedTaskValue: '',
+    isAddTaskDialogVisible: false,
+    newTaskValue: ''
+}
 export default class SubtaskContainer extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ...initState
+        }
+    }
+
+    /**
+     * Renders the input dialog for editing a subtask
+     */
+    renderEditInputDialog = (index, subtask) => {
+        return <InputDialog
+            title='Edit Subtask'
+            inputs={{ subtask: this.state.editedTaskValue }}
+            isVisible={this.state.isEditTaskDialogVisible}
+            onSaveFunc={() => {
+                this.props.onEdit(index, { ...subtask, value: this.state.editedTaskValue });
+                this.setState({ isEditTaskDialogVisible: false, editedTaskValue: '' });
+            }}
+            onChangeFunc={(field, newInput) => {
+                this.setState({ editedTaskValue: newInput })
+            }}
+            onCancelFunc={() => {
+                this.setState({ editedTaskValue: '', isEditTaskDialogVisible: false });
+            }}
+        />
+    }
+
+    /**
+     * Renders the dialog for creating new subtasks
+     */
+    renderNewTaskInputDialog = () => {
+        return <InputDialog
+            title='Create New Subtask'
+            inputs={{ subtask: this.state.newTaskValue }}
+            isVisible={this.state.isAddTaskDialogVisible}
+            onSaveFunc={() => {
+                this.props.onAdd({ isChecked: false, value: this.state.newTaskValue });
+                this.setState({ isAddTaskDialogVisible: false, newTaskValue: '' });
+            }}
+            onChangeFunc={(field, newInput) => {
+                this.setState({ newTaskValue: newInput })
+            }}
+            onCancelFunc={() => {
+                this.setState({ newTaskValue: '', isAddTaskDialogVisible: false });
+            }}
+        />
+    }
+
+    /**
+     * Renders the header of the subtasks container.
+     * Contains the overall label and add new task button
+     */
+    renderSubtaskHeader = () => {
+        return <View style={styles.subtaskHeader}>
+            <Text style={styles.label}>Subtasks</Text>
+            <Button
+                title='Add New Subtask'
+                type='clear'
+                icon={
+                    <Icon
+                        name="ios-add"
+                        size={20}
+                        color='#007aff'
+                    />
+                }
+                iconRight
+                onPress={() => { this.setState({ isAddTaskDialogVisible: true }) }}
+            />
+        </View>
+    }
+
+    /**
+     * Renders the edit and delete buttons of 
+     * each subtask
+     */
+    renderSubtaskButtons = (index, subtask) => {
+        return <View style={styles.buttonContainer}>
+            <Button
+                icon={
+                    <Icon
+                        name="ios-trash"
+                        size={35}
+                        color='#ff3b30'
+                    />}
+                type='clear'
+                onPress={() => { this.props.onDelete(index) }}
+            />
+            <Button
+                icon={
+                    <Icon
+                        raised
+                        name="md-create"
+                        size={35}
+                        color='#007aff'
+                    />}
+                type='clear'
+                onPress={() => {
+                    this.setState({ editedTaskValue: subtask.value, isEditTaskDialogVisible: true });
+                }}
+            />
+        </View>
+    }
+
     render() {
-        const { subtasks } = this.props;
+        const { subtasks, onEdit } = this.props;
         return (
             <View>
-                <View style={styles.subtaskHeader}>
-                    <Text style={styles.label}>Subtasks</Text>
-                    <Button
-                        title='Add New Subtask'
-                        type='clear'
-                    icon={
-                        <Icon
-                            name="ios-add"
-                            size={20}
-                            color='#007aff'
-                        />
-                    }
-                    iconRight
-                    />
-                </View>
+                {this.renderSubtaskHeader()}
                 <View style={styles.subtaskContainer}>
                     {
-                        subtasks.length ? subtasks.map((field, key) => {
+                        subtasks.length ? subtasks.map((subtask, index) => {
+                            return (
+                                <View style={styles.subtask} key={index}>
+                                    <CheckBox
+                                        title={subtask.value}
+                                        checked={subtask.isChecked}
+                                        containerStyle={{ width: 200 }}
+                                        onIconPress={() => {
+                                            onEdit(index, {
+                                                ...subtask, isChecked: !subtask.isChecked
+                                            });
+                                        }}
+                                        onPress={() => {
+                                            onEdit(index, {
+                                                ...subtask, isChecked: !subtask.isChecked
+                                            });
+                                        }}
+                                    />
 
-                        }) :
+                                    {this.renderSubtaskButtons(index, subtask)}
+                                    {this.renderEditInputDialog(index, subtask)}
 
-                        <Text style={styles.noSubtasksText}>
-                            This task currently has no subtasks
-                        </Text>
+                                </View>
+                            );
+                        })
+                            :
+
+                            <Text style={styles.noSubtasksText}>
+                                This task currently has no subtasks
+                            </Text>
                     }
-
                 </View>
+                {this.renderNewTaskInputDialog()}
             </View>
         );
     }
@@ -51,5 +172,7 @@ SubtaskContainer.propTypes = {
             value: PropTypes.string.isRequired,
         }).isRequired
     ).isRequired,
-
+    onDelete: PropTypes.func.isRequired,
+    onEdit: PropTypes.func.isRequired,
+    onAdd: PropTypes.func.isRequired
 }
