b832bbac804c9a440f5af5cb7a72e6ead2c3ebb9
Deanne Alabastro <aladeanra@gmail.com>
2019-09-23 15:46:35 +1200

    move rendering of buttons to function and implement list title editing

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -7,13 +7,16 @@ import InputDialog from '../InputDialog';
 import styles from './accStyles'
 
 const initState = {
-    isExpanded: false
+    isExpanded: false,
+    isEditListTitleDialogVisible: false,
+    title: ''
 }
 export default class Accordion extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            ...initState
+            ...initState,
+            title: this.props.title
         }
     }
 
@@ -26,7 +29,7 @@ export default class Accordion extends Component {
             },
             (buttonIndex) => {
                 if (buttonIndex === 0) {
-                    onTitleEdit();
+                    this.setState({ isEditListTitleDialogVisible: true });
                 } else if (buttonIndex === 1) {
                     onListDelete();
                 } else if (buttonIndex === 2) {
@@ -35,38 +38,70 @@ export default class Accordion extends Component {
             },
         );
     }
+
+    renderEditTitleDialog = () => {
+        if (this.props.hasOptions) {
+            return <InputDialog
+                title='Edit List Title'
+                inputs={{ title: this.state.title }}
+                isVisible={this.state.isEditListTitleDialogVisible}
+                onSaveFunc={() => {
+                    this.props.onTitleEdit(this.props.listId, { title: this.state.title });
+                    this.setState({ isEditTaskDialogVisible: false });
+                }}
+                onChangeFunc={(field, newInput) => {
+                    this.setState({ title: newInput })
+                }}
+                onCancelFunc={() => {
+                    this.setState({ title: this.props.title, isEditListTitleDialogVisible: false });
+                }}
+            />
+        }
+
+    }
+
+    renderIconButtons = () => {
+        return <View style={styles.buttonContainer}>
+            <Icon
+                name={this.state.isExpanded ? "ios-arrow-up" : "ios-arrow-down"}
+                size={25}
+                color='#43484d'
+            />
+            {this.props.hasOptions ?
+                <Button
+                    type="clear"
+                    icon={<Icon
+                        name="md-more"
+                        size={25}
+                        color='#007aff'
+                    />}
+                    onPress={() => {
+                        this.showActionSheet();
+                    }}
+                />
+                : null}
+
+            {this.renderEditTitleDialog()}
+        </View>
+    }
+
+
     render() {
         const { title, hasOptions, listId, onListDelete, onListItemsDelete, onTitleEdit } = this.props;
-        const {isExpanded} = this.state;
+        const { isExpanded } = this.state;
         return (
-            <TouchableOpacity 
-            style={styles.accordionContainer}
-            onPress={()=> {this.setState({isExpanded: !isExpanded})}}
-            >
-                <Text style={styles.label}>{title}</Text>
-                <View style={styles.buttonContainer}>
-                    <Icon
-                        name={isExpanded ? "ios-arrow-up" : "ios-arrow-down"}
-                        size={25}
-                        color='#43484d'
-                    />
-                    {hasOptions ?
-                        <Button
-                            type="clear"
-                            icon={<Icon
-                                name="md-more"
-                                size={25}
-                                color='#007aff'
-                            />}
-                            onPress={() => {
-                                this.showActionSheet();
-                            }}
-                        />
-                        : null}
-
-
-                </View>
-            </TouchableOpacity>
+            <>
+                <TouchableOpacity
+                    style={styles.accordionContainer}
+                    onPress={() => { this.setState({ isExpanded: !isExpanded }) }}
+                >
+                    <Text style={styles.label}>{title}</Text>
+                    {this.renderIconButtons()}
+                </TouchableOpacity>
+                <>
+                    {this.renderEditTitleDialog()}
+                </>
+            </>
         );
     }
 }
@@ -76,7 +111,6 @@ Accordion.propTypes = {
         title: PropTypes.string.isRequired
     })).isRequired,
     title: PropTypes.string.isRequired,
-    expanded: PropTypes.bool.isRequired,
     hasOptions: PropTypes.bool.isRequired,
     onTitleEdit: PropTypes.func.isRequired,
     onListDelete: PropTypes.func.isRequired,
