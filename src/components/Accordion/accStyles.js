4a7a6bc7c8cf6598f1c84ef66ae981bce4c68b1f
Deanne Alabastro <aladeanra@gmail.com>
2019-09-23 17:44:25 +1200

    create accordion stylesheet

---
+++ src/components/Accordion/accStyles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+    accordionContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: 50,
+        justifyContent: 'space-between',
+        margin: 10,
+        paddingHorizontal: 10,
+        borderBottomColor: '#86939e',
+        borderBottomWidth: 1,
+    },
+    expandedColor: {
+        backgroundColor: '#f0f4f7', 
+        color: '#79838a'
+    },
+    label: {
+        fontSize: 20,
+    },
+    subtaskContainer: {
+        margin: 20,
+    },
+    noItemsText: {
+        color: '#9EA0A4',
+        fontSize: 16,
+        textAlign: 'center'
+    },
+    subtask: {
+        flexDirection: 'row',  
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    buttonContainer: {
+        flexDirection: 'row',  
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: 70,
+    }
+});
