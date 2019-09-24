998179f308e312a1e996eecf88ff3ab3ecb77f59
Deanne Alabastro <aladeanra@gmail.com>
2019-09-24 22:48:29 +1200

    create fab for creating new tasks

---
+++ src/components/CreateTaskFab.js
@@ -0,0 +1,32 @@
+import React, { Component, } from "react";
+import PropTypes from 'prop-types';
+import { Icon } from "react-native-elements";
+
+export default class CreateTaskFab extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<Icon
+            name='md-create'
+            type='ionicon'
+            reverse
+            color='#007aff'
+            size={28}
+            raised
+            containerStyle={{
+                position: 'absolute',
+                bottom: 25,
+                right: 23,
+            }}
+            onPress={()=>{this.props.navigation.navigate('CreateTask');}}
+        />);
+    }
+}
+
+CreateTaskFab.propTypes = {
+    navigation: PropTypes.any.isRequired
+}
+
+
